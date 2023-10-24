362054b0a69128aed58189e5ef3ee22f68f90591
suheirelsayyed <sayyed.suheir.auce@gmail.com>
2023-10-24 21:01:06 +0300

    main.py
    update main function

--- Assigment3/main.py
+++ Assigment3/main.py
@@ -1,1 +1,6 @@
-print("hello world!")
+def main():
+    #Display menu to user
+    print("Please choose from menu below:\n 1.Add Matrices\n2.Check Rotation\n3.Invert Dictionary\n4.Convert Matrix to Dictionary\n5.Check Palindrome\n6.Search for an Element & Merge Sort\n7.Exit\n")
+    
+
+main()
