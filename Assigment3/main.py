44b677611f784b490b4de99704de2bcab3cc7517
suheirelsayyed <sayyed.suheir.auce@gmail.com>
2023-10-24 21:29:50 +0300

    main function update
    adding functions name

--- Assigment3/main.py
+++ Assigment3/main.py
@@ -1,6 +1,6 @@
 def main():
     #Display menu to user
-    print("Please choose from menu below:\n 1.Add Matrices\n2.Check Rotation\n3.Invert Dictionary\n4.Convert Matrix to Dictionary\n5.Check Palindrome\n6.Search for an Element & Merge Sort\n7.Exit\n")
+    print("Please choose from menu below:\n1.Add Matrices\n2.Check Rotation\n3.Invert Dictionary\n4.Convert Matrix to Dictionary\n5.Check Palindrome\n6.Search for an Element & Merge Sort\n7.Exit\n")
     
 
 main()
